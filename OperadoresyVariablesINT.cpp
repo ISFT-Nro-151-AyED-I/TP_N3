fc33f7fe8f8741fcf3802db79587f736d11038f4
davidhernanbravo <bdavidhernan@gmail.com>
2025-04-23 05:44:04 -0300

    TP_N3

--- OperadoresyVariablesINT.cpp
+++ OperadoresyVariablesINT.cpp
@@ -7,20 +7,19 @@
         c. Modificar el programa anterior para que usen variables double. ¿Qué diferencia nota? ¿Por qué? */
 
 #include <iostream>
-#include <iomanip>
                 
 using namespace std;
                 
 int main()
-{
-    int largo = 5, ancho = 4, alto = 2.5, volumenHabitacion = largo * ancho * alto;
-   
-    cout << "El volumen de la habitacion es: " << volumenHabitacion << " metros cubicos" << endl;
-                    
-        
-    int radio = 14.5, alturaCono = 26.79, pi = 3.14, volumenCono = (pi * radio * radio * alturaCono) / 3;
+    {
+        int largo = 5, ancho = 4, alto = 2.5, volumenHabitacion = largo * ancho * alto;
     
-    cout << "El volumen del cono es: " << fixed << setprecision(2) << volumenCono << " unidades cubicas" << endl;
-                    
-    return 0;
-}
+        cout << "El volumen de la habitacion es: " << volumenHabitacion << " metros cubicos" << endl;
+                        
+            
+        int radio = 14.5, alturaCono = 26.79, pi = 3.14, volumenCono = (pi * radio * radio * alturaCono) / 3;
+        
+        cout << "El volumen del cono es: " << volumenCono << " unidades cubicas" << endl;
+                        
+        return 0;
+    }
